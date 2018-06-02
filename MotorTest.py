bd301a959f98bbd2be75993a7f7e249562288aa6
Naaro <36118977+Naaro@users.noreply.github.com>
2018-06-02 14:11:43 -0700

    Update MotorTest.py

--- MotorTest.py
+++ MotorTest.py
@@ -4,7 +4,11 @@ import time
 import os
 import MotorControl as MC
 
+print('Run Setup')
+I = input('')
 MC.Setup(38,29,35,26)
+print('Configuring and starting Motors')
+I = input('')
 MC.Start()
 
 Strength = 0
